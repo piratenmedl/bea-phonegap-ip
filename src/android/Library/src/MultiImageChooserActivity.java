a57ebcbca7909955a547b8536fd0d444161339a8
piratenmedl <mail@bealisa.com>
2018-03-19 15:51:46 +0100

    deutsch

--- src/android/Library/src/MultiImageChooserActivity.java
+++ src/android/Library/src/MultiImageChooserActivity.java
@@ -197,8 +197,8 @@ public class MultiImageChooserActivity extends AppCompatActivity implements
         if (maxImages == 0 && isChecked) {
             isChecked = false;
             new AlertDialog.Builder(this)
-                    .setTitle("Maximum " + maxImageCount + " Photos")
-                    .setMessage("You can only select " + maxImageCount + " photos at a time.")
+                    .setTitle("Maximal " + maxImageCount + " Bilder")
+                    .setMessage("Du kann nur " + maxImageCount + " Bilder markieren.")
                     .setPositiveButton("OK", new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int which) {
                             dialog.cancel();
